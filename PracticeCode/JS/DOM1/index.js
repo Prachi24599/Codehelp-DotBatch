3b7b6ea6ef6e88b3c3d2c802fc7028495876471b
Prachi24599 <prachi24599@gmail.com>
2023-03-08 22:47:19 +0530

    DOM class1

---
+++ PracticeCode/JS/DOM1/index.js
@@ -0,0 +1,13 @@
+let container = document.getElementById("container");
+console.log(container);
+let child = document.getElementById("d");
+console.log(child);
+
+let name1 = container.firstElementChild.nodeName;
+console.log(name1);
+
+let header = document.getElementsByClassName("heading")[0];
+console.log(header);
+
+let section = document.querySelector(".section");
+console.log(section);
