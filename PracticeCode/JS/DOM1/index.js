4ad3b575de1e112bd4a138a93709b681b8a9d0c5
Prachi24599 <prachi24599@gmail.com>
2023-03-09 20:17:15 +0530

    dom2 added

--- PracticeCode/JS/DOM1/index.js
+++ PracticeCode/JS/DOM1/index.js
@@ -1,13 +1,28 @@
-let container = document.getElementById("container");
-console.log(container);
-let child = document.getElementById("d");
-console.log(child);
+// let container = document.getElementById("container");
+// console.log(container);
+// let child = document.getElementById("d");
+// console.log(child);
 
-let name1 = container.firstElementChild.nodeName;
-console.log(name1);
+// let name1 = container.firstElementChild.nodeName;
+// console.log(name1);
 
-let header = document.getElementsByClassName("heading")[0];
-console.log(header);
+// let header = document.getElementsByClassName("heading")[0];
+// console.log(header);
 
-let section = document.querySelector(".section");
-console.log(section);
+// let section = document.querySelector(".section");
+// console.log(section);
+
+function myFun() {
+  console.log("Event Listerner Testing");
+}
+document.addEventListener("click", myFun);
+//We need to use same function in removeEventListener which we have used in addEventListener
+document.removeEventListener("click", myFun);
+
+document.addEventListener("click", function () {
+  console.log("This is diff funcion");
+});
+//these 2 listener function are different because fun are objects in js and they are created on diff memory location
+document.removeEventListener("click", function () {
+  console.log("This is diff funcion");
+});
