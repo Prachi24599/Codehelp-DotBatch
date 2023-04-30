4014d68d00040d18467c79f86f30d0bb96ebc06a
Prachi24599 <prachi24599@gmail.com>
2023-05-01 01:04:29 +0530

    challenge1 solution

--- PracticeCode/JS/10-Functions/challenge.js
+++ PracticeCode/JS/10-Functions/challenge.js
@@ -22,12 +22,19 @@ Here are your tasks:
 	 Make sure to check if the input is a number and if the number makes sense 
 	 (e.g answer 52 wouldn't make sense, right?)
 2. Call this method whenever the user clicks the "Answer poll" button.
-3. Create a method 'displayResults' which displays the poll results. The method takes a string as an input (called 'type'), which can be either 'string' or 'array'. If type is 'array', simply display the results array as it is, using console.log(). This should be the default option. If type is 'string', display a string like "Poll results are 13, 2, 4, 1". 
+3. Create a method 'displayResults' which displays the poll results.
+ The method takes a string as an input (called 'type'), 
+ which can be either 'string' or 'array'. If type is 'array', 
+ simply display the results array as it is, using console.log(). 
+ This should be the default option. If type is 'string', display a string like 
+ "Poll results are 13, 2, 4, 1". 
 4. Run the 'displayResults' method at the end of each 'registerNewAnswer' method call.
 
 HINT: Use many of the tools you learned about in this and the last section 😉
 
-BONUS: Use the 'displayResults' method to display the 2 arrays in the test data. Use both the 'array' and the 'string' option. Do NOT put the arrays in the poll object! So what shoud the this keyword look like in this situation?
+BONUS: Use the 'displayResults' method to display the 2 arrays in the test data.
+ Use both the 'array' and the 'string' option. Do NOT put the arrays in the poll object! 
+ So what shoud the this keyword look like in this situation?
 
 BONUS TEST DATA 1: [5, 2, 3]
 BONUS TEST DATA 2: [1, 5, 3, 9, 6, 1]
@@ -41,11 +48,39 @@ const poll = {
   // This generates [0, 0, 0, 0]. More in the next section 😃
   answers: new Array(4).fill(0),
   registerNewAnswer() {
-    const ans = prompt(`  What is your favourite programming language?
-		0: JavaScript
-		1: Python
-		2: Rust
-		3: C++
-		(Write option number)`);
+    //get answer
+    //console.log(this);
+    const answer = Number(
+      prompt(
+        `${this.question}\n${this.options.join('\n')}\n(Write option number)`
+      )
+    );
+    console.log(answer);
+    typeof answer === 'number' &&
+      answer >= 0 &&
+      answer < this.options.length &&
+      this.answers[answer]++;
+
+    this.displayResult();
+    this.displayResult('string');
+  },
+  //default type set to array
+  displayResult(type = 'array') {
+    if (type === 'array') {
+      console.log(this.answers);
+    } else if (type === 'string') {
+      console.log(`Poll results are ${this.answers.join(', ')}`);
+    }
   },
 };
+// poll.registerNewAnswer();
+
+document
+  .querySelector('.poll')
+  .addEventListener('click', poll.registerNewAnswer.bind(poll));
+
+// BONUS TEST DATA 1: [5, 2, 3]
+// BONUS TEST DATA 2: [1, 5, 3, 9, 6, 1]
+
+poll.displayResult.call({ answers: [5, 2, 3] }, 'string');
+poll.displayResult.call({ answers: [1, 5, 3, 9, 6, 1] });
