e8fdb1748cd3a2ae02b9eab9b314a38dc8fc0028
Prachi24599 <prachi24599@gmail.com>
2023-05-01 13:49:03 +0530

    closures examples

--- PracticeCode/JS/10-Functions/script2.js
+++ PracticeCode/JS/10-Functions/script2.js
@@ -36,4 +36,41 @@ booker();
 booker();
 booker();
 
-console.dir(booker);
+// console.dir(booker);
+
+//Examples 1
+let f;
+const g = function () {
+  const a = 23;
+  f = function () {
+    console.log(a * 2);
+  };
+};
+
+const h = function () {
+  const b = 10;
+  f = function () {
+    console.log(b * 2);
+  };
+};
+
+g(); //f - assigned to fun in g
+f(); //46
+console.dir(f);
+
+h(); // f - reassigned to fun in h
+f(); //20
+console.dir(f);
+
+//Example 2
+const boardPassengers = function (n, wait) {
+  const perGroup = n / 3;
+
+  setTimeout(function () {
+    console.log(`We are now boarding all ${n} passengers`);
+    console.log(`There are 3 groups, each with ${perGroup} passengers`);
+  }, wait * 1000);
+
+  console.log(`Will start boarding in ${wait} seconds`);
+};
+boardPassengers(180, 3);
