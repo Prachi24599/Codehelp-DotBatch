565aa8e2f4e09424dfd0ba859bcf08b745686920
Prachi24599 <prachi24599@gmail.com>
2023-05-11 21:42:51 +0530

    constructor function

--- PracticeCode/JS/14-OOP/script.js
+++ PracticeCode/JS/14-OOP/script.js
@@ -1,1 +1,14 @@
 'use strict';
+
+function Person(firstName, birthYear) {
+  this.firstName = firstName;
+  this.birthYear = birthYear;
+}
+const prachi = new Person('Prachi', 1999);
+console.log(prachi);
+
+//This is what happens when we call a constructor function with new keyword
+// 1. New {} - Empty Object is created
+// 2. function is called, this = {} - this points to empty object
+// 3. {} linked to a prototype
+// 4. function automatically return {}
