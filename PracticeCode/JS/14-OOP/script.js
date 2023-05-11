377de8eae975ab20e52e74fd2f4cb35aba9f104e
Prachi24599 <prachi24599@gmail.com>
2023-05-11 22:36:18 +0530

    creating instance using constructor fun

--- PracticeCode/JS/14-OOP/script.js
+++ PracticeCode/JS/14-OOP/script.js
@@ -1,10 +1,19 @@
 'use strict';
+//Constructor Function
+//func declaration or function expression can be used to create cf
 
 function Person(firstName, birthYear) {
+  //Instance Properties
   this.firstName = firstName;
   this.birthYear = birthYear;
+
+  //Never do this
+  // this.calcAge = function () {
+  //   console.log(2037 - this.birthYear);
+  // };
 }
 const prachi = new Person('Prachi', 1999);
+// prachi is an instance of Person
 console.log(prachi);
 
 //This is what happens when we call a constructor function with new keyword
@@ -12,3 +21,9 @@ console.log(prachi);
 // 2. function is called, this = {} - this points to empty object
 // 3. {} linked to a prototype
 // 4. function automatically return {}
+
+const pinku = new Person('Pinku', 1998);
+const om = new Person('Om', 2004);
+console.log(pinku, om);
+
+console.log(prachi instanceof Person); //true
