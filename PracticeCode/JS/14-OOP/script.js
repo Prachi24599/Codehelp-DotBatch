9000cb7e80d8f2f20bb21388d202bdda258c1ff3
Prachi24599 <prachi24599@gmail.com>
2023-05-12 19:53:45 +0530

    Prototypal Inheritance on built in Objects

--- PracticeCode/JS/14-OOP/script.js
+++ PracticeCode/JS/14-OOP/script.js
@@ -19,7 +19,7 @@ console.log(prachi);
 //This is what happens when we call a constructor function with new keyword
 // 1. New {} - Empty Object is created
 // 2. function is called, this = {} - this points to empty object
-// 3. {} linked to a prototype
+// 3. {} - new obj linked to a constructor func prototype property
 // 4. function automatically return {}
 
 const pinku = new Person('Pinku', 1998);
@@ -38,7 +38,6 @@ prachi.calcAge();
 pinku.calcAge();
 om.calcAge();
 
-console.log(prachi.__proto__);
 console.log(prachi.__proto__ === Person.prototype);
 
 console.log(Person.prototype.isPrototypeOf(prachi)); //true
@@ -52,3 +51,25 @@ console.log(prachi.species, pinku.species);
 
 console.log(prachi.hasOwnProperty('firstName')); //true
 console.log(prachi.hasOwnProperty('species')); //false
+
+//Prototypal Inheritance on Built-in Objects
+console.log(prachi.__proto__); //Person Prototype
+console.log(prachi.__proto__.__proto__); //Object.prototype - top of prototype chain
+console.log(prachi.__proto__.__proto__.__proto__); //null
+console.dir(Person.prototype.constructor); //point back at cf Person
+
+const arr = [3, 4, 5, 4, 7, 2, 3, 8, 9]; // [] === new Array
+console.log(arr.__proto__);
+console.log(arr.__proto__ === Array.prototype);
+console.log(arr.__proto__.__proto__);
+
+Array.prototype.unique = function () {
+  return [...new Set(this)];
+};
+console.log(arr.unique());
+console.log(arr.__proto__);
+console.log(Array.prototype);
+
+const h1 = document.querySelector('h1');
+console.dir(h1);
+console.dir(x => x + 1);
