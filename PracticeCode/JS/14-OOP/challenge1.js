7c560eb9a881a323a3dc99b885e8d4ff65327c31
Prachi24599 <prachi24599@gmail.com>
2023-05-12 20:15:28 +0530

    Challenge1

---
+++ PracticeCode/JS/14-OOP/challenge1.js
@@ -0,0 +1,39 @@
+// Coding Challenge #1
+
+/* 
+1. Use a constructor function to implement a Car. A car has a make and a speed property. 
+The speed property is the current speed of the car in km/h;
+2. Implement an 'accelerate' method that will increase the car's speed by 10, and log 
+the new speed to the console;
+3. Implement a 'brake' method that will decrease the car's speed by 5, and log the 
+new speed to the console;
+4. Create 2 car objects and experiment with calling 'accelerate' and 'brake'
+multiple times on each of them.
+
+DATA CAR 1: 'BMW' going at 120 km/h
+DATA CAR 2: 'Mercedes' going at 95 km/h
+
+GOOD LUCK 😀
+*/
+
+const Car = function (make, speed) {
+  this.make = make;
+  this.speed = speed;
+};
+Car.prototype.accelerate = function () {
+  this.speed += 10;
+  console.log(`${this.make} is going at ${this.speed}km/hr`);
+};
+Car.prototype.brake = function () {
+  this.speed -= 5;
+  console.log(`${this.make} is going at ${this.speed}km/hr`);
+};
+const bmw = new Car('BMW', 120);
+const mercedes = new Car('Mercedes', 95);
+bmw.accelerate();
+bmw.accelerate();
+bmw.brake();
+
+mercedes.accelerate();
+mercedes.brake();
+mercedes.accelerate();
