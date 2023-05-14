ce10b3f85bc893ee52d0ab1fd7121c6ed68c7a1d
Prachi24599 <prachi24599@gmail.com>
2023-05-14 23:55:03 +0530

    Class Example

--- PracticeCode/JS/14-OOP/script4.js
+++ PracticeCode/JS/14-OOP/script4.js
@@ -85,3 +85,39 @@ const jay = Object.create(studentProto);
 jay.init('Jay', 1990, 'Computer');
 jay.introduce();
 jay.calcAge();
+
+//Class Example
+class Account {
+  constructor(owner, currency, pin) {
+    this.owner = owner;
+    this.currency = currency;
+    this.pin = pin;
+    this.movements = [];
+    this.locale = navigator.language;
+    console.log(`Thanks for opening an account ${owner}`);
+  }
+  deposit(val) {
+    this.movements.push(val);
+  }
+  withdraw(val) {
+    this.deposit(-val);
+  }
+  approveLoan(val) {
+    return val;
+  }
+  requestLoan(val) {
+    if (this.approveLoan(val)) {
+      this.deposit(val);
+      console.log('Loan approved');
+    }
+  }
+}
+const acc1 = new Account('Prachi', 'IND', 2400);
+// acc1.movements.push(450);
+// acc1.movements.push(-100);
+acc1.deposit(500);
+acc1.withdraw(350);
+console.log(acc1);
+console.log(acc1.pin); // We should not be able to access this outside of class so we need data encapsulation
+acc1.requestLoan(1000);
+acc1.approveLoan(2000);
