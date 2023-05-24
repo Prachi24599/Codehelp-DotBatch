885f0f623b55d0ec7e0bd03fef9b218ef6bd8182
Prachi24599 <prachi24599@gmail.com>
2023-05-24 14:05:04 +0530

    building simple promise

--- PracticeCode/JS/16-Asynchronous/script2.js
+++ PracticeCode/JS/16-Asynchronous/script2.js
@@ -1,4 +1,7 @@
 'use strict';
+
+//Event Loop in Practice
+/*
 console.log('Test Start');
 setTimeout(() => console.log('0 Sec Timer'), 0);
 Promise.resolve('Resolved Promise 1').then(res => console.log(res));
@@ -8,3 +11,20 @@ Promise.resolve('Resolved promise 2').then(res => {
   console.log(res);
 });
 console.log('Test End');
+*/
+
+//Building a Simple Promise
+//Fulfilled Promise - Win Lottery
+//Rejected Promise - loose
+const lotteryPromise = new Promise(function (resolve, reject) {
+  console.log('Lottery draw is happening🔮');
+  setTimeout(function () {
+    if (Math.random() >= 0.5) {
+      resolve('You Win 💰💸');
+    } else {
+      reject(new Error('You Loose your Money 💩'));
+    }
+  }, 2000);
+});
+
+lotteryPromise.then(res => console.log(res)).catch(err => console.error(err));
