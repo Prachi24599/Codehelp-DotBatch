9e75189d52f8be90437f03ab29dd303f5c4ebf59
Prachi24599 <prachi24599@gmail.com>
2023-05-31 13:55:27 +0530

    Promisifying the Geolocation API

--- PracticeCode/JS/16-Asynchronous/script2.js
+++ PracticeCode/JS/16-Asynchronous/script2.js
@@ -69,3 +69,21 @@ wait(1)
 
 Promise.resolve('abc').then(x => console.log(x));
 Promise.reject(new Error('Problem!')).catch(x => console.error(x));
+
+//Promisifying the Geolocation API
+// navigator.geolocation.getCurrentPosition(
+//   position => console.log(position),
+//   err => console.error(err)
+// );
+
+const getPosition = function () {
+  return new Promise(function (resolve, reject) {
+    // navigator.geolocation.getCurrentPosition(
+    //   position => resolve(position),
+    //   err => reject(err)
+    // );
+    navigator.geolocation.getCurrentPosition(resolve, reject);
+  });
+};
+
+getPosition().then(pos => console.log(pos));
