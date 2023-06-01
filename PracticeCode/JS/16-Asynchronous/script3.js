fb4a2367aedcb541c5a490796bee50c7cdebbabc
Prachi24599 <prachi24599@gmail.com>
2023-06-01 21:27:44 +0530

    running multiple promises one after another

--- PracticeCode/JS/16-Asynchronous/script3.js
+++ PracticeCode/JS/16-Asynchronous/script3.js
@@ -52,3 +52,26 @@ whereAmI('India')
 // } catch (err) {
 //   alert(err);
 // }
+
+const getJSON = function (url, errorMsg = 'Something went wrong') {
+  return fetch(url).then(response => {
+    if (!response.ok) throw new Error(`${errorMsg} ${response.status}`);
+    return response.json();
+  });
+};
+
+//Running Promises in Parallel
+const get3Countries = async function (c1, c2, c3) {
+  try {
+    // const res1 = await fetch(`https://restcountries.com/v3.1/name/${c1}`);
+    // const data1 = await res1.json();
+    const [data1] = await getJSON(`https://restcountries.com/v3.1/name/${c1}`);
+    const [data2] = await getJSON(`https://restcountries.com/v3.1/name/${c2}`);
+    const [data3] = await getJSON(`https://restcountries.com/v3.1/name/${c3}`);
+
+    console.log(data1.capital, data2.capital, data3.capital);
+  } catch (err) {
+    console.error(err);
+  }
+};
+get3Countries('Japan', 'China', 'India');
