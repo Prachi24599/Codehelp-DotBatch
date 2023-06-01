4a60bf38681ea567d2a69414cdd6f32363457e3f
Prachi24599 <prachi24599@gmail.com>
2023-06-01 21:51:37 +0530

    Running promises in parallel

--- PracticeCode/JS/16-Asynchronous/script3.js
+++ PracticeCode/JS/16-Asynchronous/script3.js
@@ -65,11 +65,20 @@ const get3Countries = async function (c1, c2, c3) {
   try {
     // const res1 = await fetch(`https://restcountries.com/v3.1/name/${c1}`);
     // const data1 = await res1.json();
-    const [data1] = await getJSON(`https://restcountries.com/v3.1/name/${c1}`);
-    const [data2] = await getJSON(`https://restcountries.com/v3.1/name/${c2}`);
-    const [data3] = await getJSON(`https://restcountries.com/v3.1/name/${c3}`);
+    // These 3 Promises will run one after another even if they are not dependent on each other
+    // const [data1] = await getJSON(`https://restcountries.com/v3.1/name/${c1}`);
+    // const [data2] = await getJSON(`https://restcountries.com/v3.1/name/${c2}`);
+    // const [data3] = await getJSON(`https://restcountries.com/v3.1/name/${c3}`);
+    // console.log(data1.capital, data2.capital, data3.capital);
 
-    console.log(data1.capital, data2.capital, data3.capital);
+    //Using Promise.all(), we are passing array of promises as input and It will return promise in output
+    //It will run all the promises in parallel
+    const data = await Promise.all([
+      getJSON(`https://restcountries.com/v3.1/name/${c1}`),
+      getJSON(`https://restcountries.com/v3.1/name/${c2}`),
+      getJSON(`https://restcountries.com/v3.1/name/${c3}`),
+    ]);
+    console.log(data.map(d => d[0].capital[0]));
   } catch (err) {
     console.error(err);
   }
