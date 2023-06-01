fd4f5ac9b8dce514ee808d4d3994285518f530a1
Prachi24599 <prachi24599@gmail.com>
2023-06-01 12:07:16 +0530

    Returning values from async function

--- PracticeCode/JS/16-Asynchronous/script3.js
+++ PracticeCode/JS/16-Asynchronous/script3.js
@@ -9,11 +9,25 @@ const whereAmI = async function (country) {
     const data = await res.json(); // It will also return a promise
     console.log(data);
     // renderCountry(data[0]);
+
+    //This returned string will become the fulfilled value of the promise that is returned by function
+    return `You are in ${data[0].capital}, ${data[0].name.common}`;
   } catch (err) {
     console.error(`${err}🔥`);
+
+    //Reject promise returned from async function
+    throw err;
   }
 };
-whereAmI('India');
+console.log('1. Will get Location');
+// const city = whereAmI('India');
+// console.log(city);
+whereAmI('India')
+  .then(city => console.log(`2. ${city}`))
+  //If we want to catch the error which occured in fun (before return)
+  //then we need to rethrow the error from catch block of fun
+  .catch(err => console.error(`2. ${err}`))
+  .finally(() => console.log('3. Finished getting location'));
 
 //Error Handling with try..catch
 // try {
